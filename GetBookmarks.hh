8c2626d917558476c9e8abe56cc6d3d654ed43ef
EDFward <edfward@gmail.com>
2016-02-14 13:37:19 -0800

    Notify slack after updating bookmarks

--- GetBookmarks.hh
+++ GetBookmarks.hh
@@ -28,6 +28,53 @@ function fetch_restaurants(string $userID = 'XzkPRDkpb5WH1KuNDkYGuA'): Set {
   return $restaurantNames;
 }
 
+$BOOKMARK_FILE_PATH = 'data/bookmarks.json';
+
+function compare_and_notify_slack(array $newBookmarks): void {
+  $oldBookmarks =
+    json_decode(file_get_contents($GLOBALS['BOOKMARK_FILE_PATH']), true);
+  // Bookmark map keyed on restaurant IDs.
+  $oldBookmarkMap = Map {};
+  $newBookmarkMap = Map {};
+
+  $oldBookmarkMap->addAll(
+    array_map($b ==> Pair {$b['id'], $b}, $oldBookmarks),
+  );
+  $newBookmarkMap->addAll(
+    array_map($b ==> Pair {$b['id'], $b}, $newBookmarks),
+  );
+
+  // Start building slack reports.
+  $report = "Booksmarks updated. Total number: {$newBookmarkMap->count()}";
+
+  $added = $newBookmarkMap->differenceByKey($oldBookmarkMap);
+  if (!$added->isEmpty()) {
+    $addedRestaurants =
+      $added->values()->map($b ==> "<{$b['url']}|{$b['name']}>");
+    $info = "Added {$added->count()} restaurant(s): ";
+    $info .= implode(", ", $addedRestaurants);
+    $report .= "\n".$info;
+  }
+
+  $deleted = $oldBookmarkMap->differenceByKey($newBookmarkMap);
+  if (!$deleted->isEmpty()) {
+    $deletedRestaurants =
+      $deleted->values()->map($b ==> "<{$b['url']}|{$b['name']}>");
+    $info = "Deleted {$deleted->count()} restaurant(s): ";
+    $info .= implode(", ", $deletedRestaurants);
+    $report .= "\n".$info;
+  }
+
+  $payload = array('text' => $report);
+
+  $ch = curl_init();
+
+  curl_setopt($ch, CURLOPT_URL, $GLOBALS['SLACK_URL']);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload));
+
+  curl_exec($ch);
+}
+
 // Setup API-related configurations.
 config();
 
@@ -46,16 +93,18 @@ foreach ($infoList as $businessInfo) {
   }
 
   // Only read necessary information.
-  $bookmark = $businessInfo['location']['coordinate'];
-  $bookmark['id'] = $businessInfo['id'];
-  $bookmark['name'] = $businessInfo['name'];
-  $bookmark['image_url'] = $businessInfo['image_url'];
-  $bookmark['url'] = $businessInfo['url'];
-  $bookmark['rating'] = $businessInfo['rating'];
-  $bookmark['categories'] =
-    array_map($p ==> $p[0], $businessInfo['categories']);
+  $b = $businessInfo['location']['coordinate'];
+  $b['id'] = $businessInfo['id'];
+  $b['name'] = $businessInfo['name'];
+  $b['image_url'] = $businessInfo['image_url'];
+  $b['url'] = $businessInfo['url'];
+  $b['rating'] = $businessInfo['rating'];
+  $b['categories'] = array_map($p ==> $p[0], $businessInfo['categories']);
 
-  array_push($bookmarks, $bookmark);
+  array_push($bookmarks, $b);
 }
 
-file_put_contents('data/bookmarks.json', json_encode($bookmarks));
+// First compare with old bookmarks.
+compare_and_notify_slack($bookmarks);
+// Then write the new bookmarks to disk.
+file_put_contents($BOOKMARK_FILE_PATH, json_encode($bookmarks));
